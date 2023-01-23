3d6398860436b0702b428ee905bbacd049c6452e
AlaaJb99 <75563685+AlaaJb99@users.noreply.github.com>
2023-01-23 19:24:00 +0200

    Delete coupon-service directory

--- coupon-service/src/main/java/com/phaseb/coupon/model/Category.java
+++
@@ -1,9 +0,0 @@
-package com.phaseb.coupon.model;
-
-import javax.persistence.Table;
-
-@Table(name = "category")
-public enum Category {
-	Food, Furniture, Clothing, Electricity, Electronics, Cosmetics, Resturant, Education, Health;
-}
-
